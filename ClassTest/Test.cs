c77c83f053a452f5de67d067fe20630af0ba358e
baklan <baklanvladislav@yandex.ru>
2024-04-12 19:11:16 +0300

    Описание ваших изменений

---
+++ ClassTest/Test.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using MOOP_Lab11;
+using NUnit.Framework;
+
+namespace ClassTest
+{
+    [TestFixture]
+    public class DecoratorPatternTests
+    {
+        [Test]
+        public void TestCheeseDecorator()
+        {
+            Sandwich sandwich = new Cheese(new Bread());
+            Assert.That(sandwich.Price, Is.EqualTo(20));
+            Assert.That(sandwich.Description, Is.EqualTo("Хлеб, сыр"));
+        }
+
+        [Test]
+        public void TestMushroomsDecorator()
+        {
+            Sandwich sandwich = new Mushrooms(new Bread());
+            Assert.That(sandwich.Price, Is.EqualTo(25));
+            Assert.That(sandwich.Description, Is.EqualTo("Хлеб, грибы"));
+        }
+
+        [Test]
+        public void TestCheeseAndMushroomsDecorator()
+        {
+            Sandwich sandwich = new Mushrooms(new Cheese(new Bread()));
+            Assert.That(sandwich.Price, Is.EqualTo(35));
+            Assert.That(sandwich.Description, Is.EqualTo("Хлеб, сыр, грибы"));
+        }
+
+        [Test]
+        public void TestVegetablesDecorator()
+        {
+            Sandwich sandwich = new Vegetables(new Bread());
+            Assert.That(sandwich.Price, Is.EqualTo(30));
+            Assert.That(sandwich.Description, Is.EqualTo("Хлеб, овощи"));
+        }
+    }
+}
